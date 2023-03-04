640eef3b5759ad63b950b5101134f14758fc6d4a
talk105 <talk105@naver.com>
2023-03-04 19:17:02 +0900

    메인 테스트 args

--- src/chap_06/_05_Overloading.java
+++ src/chap_06/_05_Overloading.java
@@ -21,6 +21,7 @@ public class _05_Overloading {
     public static void main(String[] args) {
         // 메소드 오버로딩 (전달값 타입이 다르거나 전달값의 개수가 다르거나 -> 다른 메소드)
         // 반환형이 다른형태는 불가능
+        System.out.println(args[0]);
         System.out.println(getPower("3")); // 3*3=9
     }
 }
