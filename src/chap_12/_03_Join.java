352abae4b06a23737057e6e39fd6a98863c94e48
talk105 <talk105@naver.com>
2023-06-06 13:29:14 +0900

    chap_12 Join

---
+++ src/chap_12/_03_Join.java
@@ -0,0 +1,33 @@
+package chap_12;
+
+import chap_12.clean.CleanRunnable;
+
+public class _03_Join {
+    public static void main(String[] args) {
+        CleanRunnable cleanRunnable = new CleanRunnable();
+        Thread thread = new Thread(cleanRunnable);
+        thread.start();
+
+        // 직원이 먼저 전부 실행 => thread.join()
+        try {
+            thread.join(2500); // 2.5초 대기
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        cleanByBoss();
+    }
+
+    public static void cleanByBoss(){
+        System.out.println("-- 사장 청소 시작 --");
+        for (int i = 1; i <= 10; i+=2) {
+            System.out.println("(사장) " + i + " 번방 청소중");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("-- 사장 청소 끝 --");
+    }
+}
